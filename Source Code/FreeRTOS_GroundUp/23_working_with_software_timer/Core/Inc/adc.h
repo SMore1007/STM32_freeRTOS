a241639c6488912b9747a2f7420b609593a29fe5
Sagar <sagarmore1007@gmail.com>
2025-05-25 12:37:26 +0530

    STM32 freeRTOS

--- Source Code/FreeRTOS_GroundUp/23_working_with_software_timer/Core/Inc/adc.h
+++
@@ -1,7 +0,0 @@
-#ifndef INC_ADC_H_
-#define INC_ADC_H_
-#include "stdint.h"
-void adc_init();
-uint32_t read_analog_sensor(void);
-
-#endif /* INC_ADC_H_ */
--- Source Code/FreeRTOS_GroundUp/29_event_group_multiple_setter/Core/Inc/uart.h
+++
@@ -1,8 +0,0 @@
-#ifndef INC_UART_H_
-#define INC_UART_H_
-#include "stdio.h"
-
-void USART2_UART_TX_Init(void);
-void USART2_UART_RX_Init(void);
-char uart2_read(void);
-#endif /* INC_UART_H_ */
